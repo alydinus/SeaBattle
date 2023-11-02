72f18137d1a53ad1b9330f8ecd9f34e710046d5a
Alydin <alykulovalydin@gmail.com>
2023-11-02 22:55:38 +0600

    Fixed some bugs about borders of every ship

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+import java.awt.image.AreaAveragingScaleFilter;
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = scanner.nextLine();
+//        String name = scanner.nextLine();
+
         String[][] matrix = new String[7][7];
         Integer[][] matrix1 = new Integer[7][7];
         String s = "*";
@@ -21,28 +23,26 @@ public class Main {
             if (x1 >= y1) {
                 matrix[x1 + i][y1] = s;
                 try {
-                    matrix1[x1 + i][Math.abs(y1 - 1)] = 2;
-                    matrix1[x1 + i][Math.abs(y1 + 1)] = 2;
-
-                    matrix1[Math.abs(x1 - 1)][y1 - 1 + i] = 2;
-                    matrix1[Math.abs(x1 + 3)][y1 - 1 + i] = 2;
-
-                    matrix1[x1 + i][y1] = 1;
+                    //ограничения слева и справа
+                    matrix1[x1 + i][Math.abs(y1 - 1)] = 9;
+                    matrix1[x1 + i][Math.abs(y1 + 1)] = 9;
+                    //сверху и снизу
+                    matrix1[Math.abs(x1 - 1)][y1 - 1 + i] = 9;
+                    matrix1[Math.abs(x1 + 3)][y1 - 1 + i] = 9;
+                    matrix1[x1 + i][y1] = 3;
                 } catch (ArrayIndexOutOfBoundsException e) {
-                    matrix1[x1 + i][y1] = 1;
+                    matrix1[x1 + i][y1] = 3;
                 }
             } else {
                 try {
                     matrix[x1][y1 + i] = s;
-                    matrix1[Math.abs(x1 - 1)][y1 + i] = 2;
-                    matrix1[Math.abs(x1 + 1)][y1 + i] = 2;
-
-                    matrix1[x1 - 1 + i][Math.abs(y1 - 1)] = 2;
-                    matrix1[x1 - 1 + i][Math.abs(y1 + 3)] = 2;
-
-                    matrix1[x1][y1 + i] = 1;
+                    matrix1[Math.abs(x1 - 1)][y1 + i] = 9;
+                    matrix1[Math.abs(x1 + 1)][y1 + i] = 9;
+                    matrix1[x1 - 1 + i][Math.abs(y1 - 1)] = 9;
+                    matrix1[x1 - 1 + i][Math.abs(y1 + 3)] = 9;
+                    matrix1[x1][y1 + i] = 3;
                 } catch (ArrayIndexOutOfBoundsException e) {
-                    matrix1[x1][y1 + i] = 1;
+                    matrix1[x1][y1 + i] = 3;
                 }
             }
         }
@@ -55,50 +55,64 @@ public class Main {
                 continue;
             }
             if (x2 >= y2) {
-                if (matrix1[Math.abs(x2 - 1)][y2] != 0) {
+                if (matrix1[Math.abs(x2 + 1)][y2] != 0) {
                     continue;
                 }
                 for (int i = 0; i < 2; i++) {
-                    try {
-                        matrix1[Math.abs(x2 - i)][y2] = 1;
-                        matrix[Math.abs(x2 - i)][y2] = s;
-                        matrix1[x2 - i][Math.abs(y2 - 1)] = 2;
-                        matrix1[x2 - i][Math.abs(y2 + 1)] = 2;
-                        for (int j = 0; j < 3; j++) {
-                            try {
-                                matrix1[x2 + 1][y2 - 1 + j] = 2;
-                                matrix1[x2 - 2][y2 - 1 + j] = 2;
-                            } catch (ArrayIndexOutOfBoundsException e) {
-                            }
-                        }
-                    } catch (ArrayIndexOutOfBoundsException e) {
+                    matrix[x2 + i][y2] = s;
+                    try{
+                        matrix1[x2 + i][Math.abs(y2 - 1)] = 9;
+                        matrix1[x2 + i][Math.abs(y2 + 1)] = 9;
+                    }catch (ArrayIndexOutOfBoundsException e){
+                        continue;
                     }
-                }
-            } else {
-                if (matrix1[x2][Math.abs(y2 - 1)] != 0) {
-                    continue;
-                }
-                for (int i = 0; i < 2; i++) {
-                    try {
-                        matrix1[x2][Math.abs(y2 - i)] = 1;
-                        matrix[x2][Math.abs(y2 - i)] = s;
-                        matrix1[Math.abs(x2 - 1)][y2 - i] = 2;
-                        matrix1[x2 + 1][y2 - i] = 2;
-                        for (int j = 0; j < 4; j++) {
-                            try {
-                                matrix1[x2 + 1 - i][y2 + 1] = 2;
-                                matrix1[x2 + 1 - i][y2 - 2] = 2;
-                            } catch (ArrayIndexOutOfBoundsException e) {
-                            }
+                    for (int j = 0; j < 3; j++){
+                        try{
+                            matrix1[x2 - 1][y2 - 1 + j] = 9;
+                        }catch (ArrayIndexOutOfBoundsException e){
+                            matrix1[x2 + i][y2] = 2;
                         }
-                    } catch (ArrayIndexOutOfBoundsException e) {
+                        try{
+                            matrix1[Math.abs(x2 + 2)][y2 - 1 + j] = 9;
+                        }catch (ArrayIndexOutOfBoundsException e){
+                            matrix1[x2 + i][y2] = 2;
+                        }
+                    }
+                    matrix1[x2 + i][y2] = 2;
+                }
 
-                    }
+            }
+            else {
+                if (matrix1[x2][Math.abs(y2 + 1)] != 0) {
+                    continue;
                 }
+                for (int i = 0; i < 2; i++) {
+                    matrix[x2][y2 + i] = s;
+                    try {
+                        matrix1[Math.abs(x2 - 1)][y2 + i] = 9;
+                        matrix1[Math.abs(x2 + 1)][y2 + i] = 9;
+                    }catch (ArrayIndexOutOfBoundsException e){
+                        matrix1[x2][y2 + i] = 2;
+                    }
+                    for (int j = 0; j < 3; j++){
+                        try{
+                            matrix1[x2 - 1 + j][Math.abs(y2 - 1)] = 9;
+                        }catch (ArrayIndexOutOfBoundsException e){
+                            matrix1[x2][y2 + i] = 2;
+                        }
+                        try{
+                            matrix1[x2 - 1 + j][y2 + 2] = 9;
+                        }catch (ArrayIndexOutOfBoundsException e){
+                            matrix1[x2][y2 + i] = 2;
+                        }
+                    }
+                    matrix1[x2][y2 + i] = 2;
+                }
+
             }
             temp++;
         }
-        //SHIPS 1x1
+//        //SHIPS 1x1
         int z = 0;
         while (z < 4) {
             int x3 = (int) Math.floor(Math.random() * 7);
@@ -109,24 +123,27 @@ public class Main {
             matrix[x3][y3] = s;
             for (int i = 0; i < 3; i++) {
                 try {
+                    matrix1[x3 - 1 + i][y3 - 1] = 9;
+                }catch (ArrayIndexOutOfBoundsException e){
                     matrix1[x3][y3] = 1;
-                    matrix1[Math.abs(x3 - 1)][y3 - 1 + i] = 2;
-                    matrix1[x3 + 1][y3 - 1 + i] = 2;
-                    matrix1[x3][Math.abs(y3 - 1)] = 2;
-                    matrix1[x3][Math.abs(y3 + 1)] = 2;
-                } catch (ArrayIndexOutOfBoundsException e) {
+                }try{
+                    matrix1[x3 - 1 + i][y3] = 9;
+                }catch (ArrayIndexOutOfBoundsException e){
+                    matrix1[x3][y3] = 1;
                 }
+                try {
+                    matrix1[x3 - 1 + i][y3 + 1] = 9;
+                }catch (ArrayIndexOutOfBoundsException e){
+                    matrix1[x3][y3] = 1;
+                }
+                matrix1[x3][y3] = 1;
             }
             z++;
         }
 
-
-
-
-
         System.out.println();
         System.out.println("Enter coordinates");
-        while (true){
+        while (true) {
             for (int i = 0; i < 7; i++) {
                 for (int j = 0; j < 7; j++) {
                     System.out.print(matrix[i][j] + " ");
@@ -135,19 +152,27 @@ public class Main {
             }
             int shotx = scanner.nextInt();
             int shoty = scanner.nextInt();
-            if (matrix1[shoty - 1][shotx -1] == 1){
+            if (matrix1[shoty - 1][shotx - 1] == 1) {
                 System.out.println("Hit");
-                matrix[shoty - 1][shotx -1] = "X";
-            }else{
+                matrix[shoty - 1][shotx - 1] = "X";
+            } else {
                 System.out.println("Miss");
             }
             System.out.println();
         }
 //        for (int i = 0; i < 7; i++) {
 //            for (int j = 0; j < 7; j++) {
+//                System.out.print(matrix[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//        for (int i = 0; i < 7; i++) {
+//            for (int j = 0; j < 7; j++) {
 //                System.out.print(matrix1[i][j]);
 //            }
 //            System.out.println();
 //        }
+
+
     }
 }
